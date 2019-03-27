d35a98ca917d6af78315419256d0564b08ac0706
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2019-03-27 11:22:41 +0900

    -

--- update_pubchem.py
+++ update_pubchem.py
@@ -5,26 +5,18 @@ from mysync import MyFTP
 ftp = MyFTP('ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/SDF')
 ftp.set_local_dir('data/pubchem')
 ls = ftp.get_file_list()
+
+import re
+
+pat = re.compile('^Compound_([0-9]+)_([0-9]+)\.sdf\.gz$')
 for name in ls:
-    # if name.startswith('Compound_00'):
-    #     continue
-    # if name.startswith('Compound_01'):
-    #     continue
-    # if name.startswith('Compound_02'):
-    #     continue
-    # if name.startswith('Compound_030'):
-    #     continue
-    # if name.startswith('Compound_031'):
-    #     continue
-    # if name.startswith('Compound_032'):
-    #     continue
-    # if name.startswith('Compound_033'):
-    #     continue
-    # if name.startswith('Compound_034'):
-    #     continue
-    # if name.startswith('Compound_035'):
-    #     continue
-    # if name.startswith('Compound_036'):
-    #     continue
+    m = pat.match(name)
+    if m is None:
+        continue
+    a = int(m.group(1))
+    b = int(m.group(2))
+    if a < 75950000:
+        print(f'{name} skip')
+        continue
     mode = 'binary' if name.endswith('.sdf.gz') else 'text'
     ftp.download(name, mode=mode)
